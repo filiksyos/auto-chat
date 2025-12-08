0c751b862ceb9bf1f36553073fb43b6075f44720
Fili <126945152+filiksyos@users.noreply.github.com>
2025-12-08 08:41:03 +0300

    Initial commit: Auto Chat MVP with intelligent AI routing

---
+++ src/preload/preload.js
@@ -0,0 +1,41 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  // Send message and get AI routing decision
+  routeMessage: (message) => ipcRenderer.invoke('route-message', message),
+  
+  // Manually change service
+  changeService: (service) => ipcRenderer.invoke('change-service', service),
+  
+  // Send text update to chat view
+  sendTextUpdate: (text) => ipcRenderer.invoke('send-text-update', text),
+  
+  // Submit message
+  submitMessage: () => ipcRenderer.invoke('submit-message'),
+  
+  // New chat
+  newChat: () => ipcRenderer.invoke('new-chat'),
+  
+  // Refresh page
+  refreshPage: () => ipcRenderer.invoke('refresh-page'),
+  
+  // Listen for service changes
+  onServiceChanged: (callback) => {
+    ipcRenderer.on('service-changed', (event, service) => callback(service));
+  },
+  
+  // Listen for text updates
+  onTextUpdate: (callback) => {
+    ipcRenderer.on('text-update', (event, text) => callback(text));
+  },
+  
+  // Listen for submit message
+  onSubmitMessage: (callback) => {
+    ipcRenderer.on('submit-message', () => callback());
+  },
+  
+  // Listen for new chat
+  onNewChat: (callback) => {
+    ipcRenderer.on('new-chat', () => callback());
+  }
+});
